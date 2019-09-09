1156699f21ed56bd49ba83314fd3479d6919c507
Matthew Zipkin <pinheadmz@gmail.com>
2019-09-09 11:05:43 -0400

    update and clean up

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const assert = require('assert');
 const bcoin = require('bcoin');
 const network = bcoin.Network.get(process.env.BCOIN_NETWORK);
@@ -16,23 +18,23 @@ const bc = new NodeClient({
   apiKey: process.env.BCOIN_API_KEY
 });
 
-(async () =>{
+(async () => {
   const info = await bc.getInfo();
   const height = info.chain.height;
 
-  for (let h = 0; h <= height; h++){
+  for (let h = 0; h <= height; h++) {
     const block = await bc.getBlock(h);
     const hash = block.hash;
 
     const filter1 = await btcd.execute('getcfilter', [hash, 0]);
-    const filter2 = await bc.execute('getcfilter', [hash, 0]);
-    assert.strictEqual(filter1, filter2);
+    const filter2 = await bc.execute('getblockfilter', [hash, 0]);
+    assert.strictEqual(filter1, filter2.filter);
 
     console.log(`filter match for height ${h}: ${filter1}`);
 
     const filterH1 = await btcd.execute('getcfilterheader', [hash, 0]);
-    const filterH2 = await bc.execute('getcfilterheader', [hash, 0]);
-    assert.strictEqual(filterH1, filterH2);
+    // const filterH2 = await bc.execute('getcfilterheader', [hash, 0]);
+    assert.strictEqual(filterH1, filter2.header);
 
     console.log(`filter header match for height ${h}: ${filterH1}`);
   }
